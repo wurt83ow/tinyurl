3f6d3963b3f8db7238600aa237f46fdeb1a8c7df
wurt83ow <wurt83ow@yandex.ru>
2023-08-05 15:04:17 +0300

    moved handlers to controllers folder

--- cmd/shortener/server/server.go
+++ cmd/shortener/server/server.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	"handler"
+	"controllers"
 	"net/http"
 	"storage"
 )
@@ -11,23 +11,8 @@ const (
 	CONN_PORT = "8080"
 )
 
-var hndlr *handler.Handler
-
-func webhook(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method == http.MethodPost {
-		hndlr.ShortenURL(w, r)
-	} else if r.Method == http.MethodGet {
-		hndlr.GetFullUrl(w, r)
-	} else {
-		// allow only post/get requests, otherwise send a 405 code
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-}
-
 func Run() error {
 	memoryStorage := storage.NewMemoryStorage()
-	hndlr = handler.NewHandler(memoryStorage)
-	return http.ListenAndServe(CONN_HOST+":"+CONN_PORT, http.HandlerFunc(webhook))
+	handler := controllers.NewHandler(memoryStorage)
+	return http.ListenAndServe(CONN_HOST+":"+CONN_PORT, http.HandlerFunc(handler.Webhook))
 }
--- internal/controllers/base.go
+++ internal/controllers/base.go
@@ -1,23 +1,40 @@
-package handler
+package controllers
 
 import (
 	"io"
 	"net/http"
 	"shorturl"
-	"storage"
 	"strings"
 )
 
-type Handler struct {
-	storage storage.Storage
+type Storage interface {
+	Insert(k string, v string) error
+	Get(k string) (string, error)
 }
 
-func NewHandler(storage storage.Storage) *Handler {
+type Handler struct {
+	storage Storage
+}
+
+func NewHandler(storage Storage) *Handler {
 	return &Handler{storage: storage}
 }
 
+func (h *Handler) Webhook(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodPost {
+		h.shortenURL(w, r)
+	} else if r.Method == http.MethodGet {
+		h.getFullUrl(w, r)
+	} else {
+		// allow only post/get requests, otherwise send a 405 code
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
 // POST
-func (h *Handler) ShortenURL(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) shortenURL(w http.ResponseWriter, r *http.Request) {
 	// установим правильный заголовок для типа данных
 	body, err := io.ReadAll(r.Body)
 	if err != nil || len(body) == 0 {
@@ -36,7 +53,10 @@ func (h *Handler) ShortenURL(w http.ResponseWriter, r *http.Request) {
 
 	// save full url to storage with the key received earlier
 	h.storage.Insert(key, string(body))
-
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	// respond to client
 	w.Header().Set("content-type", "text/plain")
 	// set code 201
@@ -45,7 +65,7 @@ func (h *Handler) ShortenURL(w http.ResponseWriter, r *http.Request) {
 }
 
 // GET
-func (h *Handler) GetFullUrl(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) getFullUrl(w http.ResponseWriter, r *http.Request) {
 	key := r.URL.Path
 	key = strings.Replace(key, "/", "", -1)
 	if len(key) == 0 {
