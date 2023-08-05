66e1dee1842078c8632c10766ee9885aa2b0ee7a
wurt83ow <wurt83ow@yandex.ru>
2023-08-05 16:23:15 +0300

    add web-hook tests

---
+++ internal/controllers/base_test.go
@@ -0,0 +1,59 @@
+package controllers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"storage"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWebhook(t *testing.T) {
+	// описываем передаваемое тело
+	url := "https://practicum.yandex.ru/"
+	requestBody := strings.NewReader(url)
+	defaultBody := strings.NewReader("")
+	// описываем ожидаемое тело ответа при успешном запросе
+	successBody := "http://example.com/nOykhckC3Od"
+
+	defaultPath := "/"
+	// описываем набор данных: метод запроса, ожидаемый код ответа, ожидаемое тело
+	testCases := []struct {
+		method       string
+		path         string
+		expectedCode int
+		expectedBody string
+		requestBody  *strings.Reader
+		location     string
+	}{
+		{method: http.MethodPost, path: defaultPath, expectedCode: http.StatusCreated, expectedBody: successBody, requestBody: requestBody},
+		{method: http.MethodGet, path: "/nOykhckC3Od", expectedCode: http.StatusTemporaryRedirect, expectedBody: "", requestBody: defaultBody, location: url},
+		{method: http.MethodPut, path: defaultPath, expectedCode: http.StatusMethodNotAllowed, expectedBody: "", requestBody: defaultBody},
+		{method: http.MethodDelete, path: defaultPath, expectedCode: http.StatusMethodNotAllowed, expectedBody: "", requestBody: defaultBody},
+	}
+
+	memoryStorage := storage.NewMemoryStorage()
+	handler := NewBaseController(memoryStorage)
+	for _, tc := range testCases {
+		t.Run(tc.method, func(t *testing.T) {
+
+			r := httptest.NewRequest(tc.method, tc.path, tc.requestBody)
+			w := httptest.NewRecorder()
+
+			// вызовем хендлер как обычную функцию, без запуска самого сервера
+			handler.Webhook(w, r)
+
+			assert.Equal(t, tc.expectedCode, w.Code, "Код ответа не совпадает с ожидаемым")
+			// проверим корректность полученного тела ответа, если мы его ожидаем
+			if tc.expectedBody != "" {
+				// assert.JSONEq помогает сравнить две JSON-строки
+				assert.Equal(t, tc.expectedBody, w.Body.String(), "Тело ответа не совпадает с ожидаемым")
+			}
+
+			assert.Equal(t, tc.location, w.Header().Get("Location"), "Заголовок Location не совпадает с ожидаемым")
+
+		})
+	}
+}
