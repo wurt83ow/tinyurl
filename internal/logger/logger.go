fa1cf6b43fe7130034ca134b7a11269764ce5cd7
wurt83ow <wurt83ow@yandex.ru>
2023-08-31 17:32:36 +0300

    remove comments

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -8,27 +8,6 @@ type Logger struct {
 	zap *zap.Logger
 }
 
-// Initialize инициализирует синглтон логера с необходимым уровнем логирования.
-// func Initialize(level string) error {
-// 	// преобразуем текстовый уровень логирования в zap.AtomicLevel
-// 	lvl, err := zap.ParseAtomicLevel(level)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	// создаём новую конфигурацию логера
-// 	cfg := zap.NewProductionConfig()
-// 	// устанавливаем уровень
-// 	cfg.Level = lvl
-// 	// создаём логер на основе конфигурации
-// 	zl, err := cfg.Build()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	// устанавливаем синглтон
-// 	Log = zl
-// 	return nil
-// }
-
 func NewLogger(level string) (*Logger, error) {
 
 	// преобразуем текстовый уровень логирования в zap.AtomicLevel
