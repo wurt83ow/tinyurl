8ee70f3667f9db66ba26a10926a0a5d43fe2c4f6
wurt83ow <wurt83ow@yandex.ru>
2023-09-13 18:45:23 +0300

    test1

--- migrations/20230913123931_create_dataurl_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS dataurl (
-	correlation_id VARCHAR(50) PRIMARY KEY, 
-	short_url TEXT,
-	original_url TEXT 	 
-	);
-CREATE UNIQUE INDEX IF NOT EXISTS uniq_url ON dataurl (original_url);
