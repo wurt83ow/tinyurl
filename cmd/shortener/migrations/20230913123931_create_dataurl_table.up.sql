e1c3837770a8157c75bf0d62e719305a065848c1
wurt83ow <wurt83ow@yandex.ru>
2023-09-24 17:14:45 +0300

    fix error iter15

--- cmd/shortener/migrations/20230913123931_create_dataurl_table.up.sql
+++ cmd/shortener/migrations/20230913123931_create_dataurl_table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS dataurl (
 	short_url TEXT,
 	user_id VARCHAR(50) NOT NULL,
 	original_url TEXT,
-	is_deleted BOOLEAN   
+	is_deleted BOOLEAN NOT NULL   
 	);
 CREATE UNIQUE INDEX IF NOT EXISTS uniq_url ON dataurl (original_url);
