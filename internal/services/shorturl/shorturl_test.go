06f2acf1be7cd202a3a65a5b07d83b758b92eb62
wurt83ow <wurt83ow@yandex.ru>
2023-11-16 18:18:23 +0300

    Covered with tests 100% shorturl package

--- internal/services/shorturl/shorturl_test.go
+++ internal/services/shorturl/shorturl_test.go
@@ -4,23 +4,27 @@ import (
 	"testing"
 )
 
-// shortURLAdress := configs.ShortURLAdress()
 func TestShorten(t *testing.T) {
-	tests := []struct { // adding slice tests
-		name           string
-		url            string
-		shortURLAdress string
-		key            string
-		shurl          string
-	}{
-		{
-			name:           "simple test #1",
-			url:            "https://practicum.yandex.ru/",
-			shortURLAdress: "http://localhost:8080/",
-			key:            "nOykhckC3Od",
-			shurl:          "http://localhost:8080/nOykhckC3Od",
-		},
+
+	type test struct {
+		name, url, shortURLAdress, key, shurl string
 	}
+
+	test1 := test{
+		name:           "simple test #1",
+		url:            "https://practicum.yandex.ru/",
+		shortURLAdress: "http://localhost:8080/",
+		key:            "nOykhckC3Od",
+		shurl:          "http://localhost:8080/nOykhckC3Od",
+	}
+
+	tests := []test{
+		test1,
+	}
+	test2 := test1
+	test2.shortURLAdress = test2.shortURLAdress[:len(test2.shortURLAdress)-1]
+	tests = append(tests, test2)
+
 	for _, test := range tests { // cycle through all tests
 		t.Run(test.name, func(t *testing.T) {
 			if key, shurl := Shorten(test.url, test.shortURLAdress); key != test.key || shurl != test.shurl {
